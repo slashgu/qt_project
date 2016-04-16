9df0fe4e882558ab753f59f4d67837e77536c7ea
Slashu <guchengslash@gmail.com>
2016-04-16 13:57:09 -0400

    display done

--- Tesla/tesla_q3/gitTest.py
+++ Tesla/tesla_q3/gitTest.py
@@ -1,12 +1,32 @@
 __author__ = 'chenggu'
 
 import os
+import re
 import time
 from git import Repo
 join = os.path.join
 
 # A repo object provides high-level access to your data
 repo = Repo('/Users/chenggu/myQt/')
+# print len(list(repo.iter_commits()))
 
-commits = list(repo.iter_commits())
-print commits
+master = repo.head.reference
+# print isinstance(repo.git.log(since = "2.weeks"), str)
+list = repo.git.log(oneline = True, graph = True).split('\n')
+levelIndicator = ['*', '|', '\\', '/']
+reGitInfo = re.compile('[*\s|\\/]+([0-9a-zA-Z]+)\s([0-9a-zA-Z\_\s]+)')
+
+count = 0
+
+for item in list:
+    # level = 0
+    # for i in levelIndicator:
+    #     level += str(item).count(i)
+    # print str(item) + " ---- level: " + str(level)
+    print str(item) + " : " + str(count)
+    count += 1
+    # print isinstance(item, unicode)
+    # if reGitInfo.match(str(item)):
+        # count += 1
+        # print reGitInfo.match(str(item)).groups()
+# print count
--- Tesla/tesla_q3/tesla_q3_ui2.py
+++ Tesla/tesla_q3/tesla_q3_ui2.py
@@ -8,6 +8,7 @@
 # WARNING! All changes made in this file will be lost!
 
 import sys
+import re
 from git import Repo
 from PyQt4 import QtCore, QtGui
 
@@ -43,8 +44,29 @@ class Ui_tesla_q3(QtGui.QWidget):
 
         # item_0 = QtGui.QTreeWidgetItem(self.treeWidget)
         # item_1 = QtGui.QTreeWidgetItem(self.treeWidget)
-        for i in range(len(commits)):
-            item = QtGui.QTreeWidgetItem(self.treeWidget)
+        i = 0
+        flag = False
+        count = 0
+        while i < len(list):
+            if not reGitInfo.match(str(list[i])):
+                i += 1
+                if count % 2 == 0:
+                    flag = True
+                    count += 1
+                else:
+                    flag = False
+                    count += 1
+            if not flag:
+                item_root = QtGui.QTreeWidgetItem(self.treeWidget)
+            else:
+                item_child = QtGui.QTreeWidgetItem(item_root)
+            i += 1
+
+            # if reGitInfo.match(str(item)):
+            #     item_1 = QtGui.QTreeWidgetItem(self.treeWidget)
+            # else:
+            #     item_2 = QtGui.QTreeWidgetItem(item_1)
+
 
         self.horizontalLayout.addWidget(self.treeWidget)
         self.verticalLayout.addLayout(self.horizontalLayout)
@@ -57,18 +79,54 @@ class Ui_tesla_q3(QtGui.QWidget):
         tesla_q3.setWindowTitle(_translate("tesla_q3", "tesla_q3", None))
         self.treeWidget.headerItem().setText(0, _translate("tesla_q3", "commits", None))
         self.treeWidget.headerItem().setText(1, _translate("tesla_q3", "comments", None))
-        self.treeWidget.headerItem().setText(2, _translate("tesla_q3", "author", None))
-        for i in range(len(commits)):
-        # self.treeWidget.topLevelItem(0).setText(0, _translate("tesla_q3", str(headCommit), None))
-        # self.treeWidget.topLevelItem(0).setText(1, _translate("tesla_q3", headCommit.message, None))
-            self.treeWidget.topLevelItem(i).setText(0, _translate("tesla_q3", str(commits[i]), None))
-            self.treeWidget.topLevelItem(i).setText(1, _translate("tesla_q3", commits[i].message, None))
-            self.treeWidget.topLevelItem(i).setText(2, _translate("tesla_q3", commits[i].author.name, None))
+        # self.treeWidget.headerItem().setText(2, _translate("tesla_q3", "author", None))
+        i = 0
+        count = 0
+        flag = False
+        parent = 0
+        children = 0
+
+        while i < len(list):
+            if not reGitInfo.match(str(list[i])):
+                i += 1
+                if count % 2 == 0:
+                    flag = True
+                    count += 1
+                    parent -= 1
+                else:
+                    flag = False
+                    children = 0
+                    count += 1
+            if not flag:
+                self.treeWidget.topLevelItem(parent).setText(0, _translate("tesla_q3", str(reGitInfo.match(str(list[i])).groups()[0]), None))
+                self.treeWidget.topLevelItem(parent).setText(1, _translate("tesla_q3", str(reGitInfo.match(str(list[i])).groups()[1]), None))
+                print reGitInfo.match(str(list[i])).groups()
+                parent += 1
+            else:
+                self.treeWidget.topLevelItem(parent).child(children).setText(0, _translate("tesla_q3", str(reGitInfo.match(str(list[i])).groups()[0]), None))
+                self.treeWidget.topLevelItem(parent).child(children).setText(1, _translate("tesla_q3", str(reGitInfo.match(str(list[i])).groups()[1]), None))
+                print reGitInfo.match(str(list[i])).groups()
+                children += 1
+
+            i += 1
+
+        # for item in list:
+        #     if reGitInfo.match(str(item)):
+        #         self.treeWidget.topLevelItem(i).setText(0, _translate("tesla_q3", str(reGitInfo.match(str(item)).groups()[0]), None))
+        #         self.treeWidget.topLevelItem(i).setText(1, _translate("tesla_q3", str(reGitInfo.match(str(item)).groups()[1]), None))
+        #         i += 1
+        #
+        #         # self.treeWidget.topLevelItem(i).setText(2, _translate("tesla_q3", commits[i].author.name, None))
+        #     else:
+        #         pass
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     repo = Repo('/Users/chenggu/myQt/')
-    commits = list(repo.iter_commits())
+    stack = []
+    list = repo.git.log(oneline = True, graph = True).split('\n')
+    reGitInfo = re.compile('[*\s|\\/]+([0-9a-zA-Z]+)\s([0-9a-zA-Z\_\s]+)')
+    # commits = list(repo.iter_commits())
     ex = Ui_tesla_q3()
     ex.show()
     sys.exit(app.exec_())
